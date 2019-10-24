e5fa4c776795291c5e8d061fe00393886b3ad8a9
miguelteran1997 <54339141+miguelteran1997@users.noreply.github.com>
2019-10-24 13:02:40 -0500

    Add files via upload

---
+++ Coach.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OffPractice.Data
+{
+    public class Coach
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CID { get; set; }
+        public string CN { get; set; }
+        public int TID { get; set; }
+        public string TN { get; set; }
+        public DateTime ReleaseDate { get; internal set; }
+    }
+}
---
+++ Edit.cshtml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using OffPractice.Data;
+
+namespace OffPractice.Pages.Boot
+{
+    public class EditModel : PageModel
+    {
+        private readonly OffPractice.Data.ApplicationDbContext _context;
+
+        public EditModel(OffPractice.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Bootleg Bootleg { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Bootleg = await _context.Bootleg.FirstOrDefaultAsync(m => m.BID == id);
+
+            if (Bootleg == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Bootleg).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BootlegExists(Bootleg.BID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool BootlegExists(int id)
+        {
+            return _context.Bootleg.Any(e => e.BID == id);
+        }
+    }
+}
